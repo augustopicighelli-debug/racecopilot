b920cc89a9177a7506dd6f4680858f46cd1cb312
Augusto Picighelli <augustopicighelli@gmail.com>
2026-04-12 17:20:23 -0300

    fix: correct nextauth handlers export

--- auth.ts
+++ auth.ts
@@ -1,11 +1,11 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
+export const { handlers, auth } = NextAuth({
   providers: [
     Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     }),
   ],
   pages: {
